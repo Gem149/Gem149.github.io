36878b68539d0cc819c9dffb2354a311151717c5
Gabby Martinez <gmleggit@gmail.com>
2019-09-27 12:10:46 -0400

    updates

--- contactusjava.js
+++ contactusjava.js
@@ -37,3 +37,11 @@ $(document).ready(function() {
       }
   })
 })
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
